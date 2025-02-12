fe2a0b5f8237d9b9be177f289852fa29d432ad66
Matias Perez <matiasperezpc@hotmail.com>
2025-02-12 17:20:54 -0300

    Update home page header text and styling

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -55,7 +55,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center pt-header h-screen bg-accent">
       <div className="flex flex-col justify-center items-center mt-10 p-4">
-        <h1 className="font-sans text-6xl uppercase font-bold mb-7">Rebels starter</h1>
+        <h1 className="font-sans text-6xl font-bold mb-7">RRv7 STARTER</h1>
         <Paragraph>
           <span className="font-bold">WARNING</span> - By using this template you accept to get:
         </Paragraph>
