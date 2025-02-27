af81852cbaa55ded8b6c22beb0407aa58b57ce0b
Matias Perez <matiasperezpc@hotmail.com>
2025-02-27 04:33:51 -0300

    Add suppressHydrationWarning to body tag to prevent React hydration warnings

--- app/root.tsx
+++ app/root.tsx
@@ -75,7 +75,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <Header />
         {children}
         <Footer />
