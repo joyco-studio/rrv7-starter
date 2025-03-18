1f3bb96a1806726ab45271921a2786d37e09254f
Matias Perez <43894343+matiasperz@users.noreply.github.com>
2025-03-18 15:11:35 -0300

    add disable to logger

--- app/lib/utils/logger.ts
+++ app/lib/utils/logger.ts
@@ -1,18 +1,41 @@
+import { useEffect, useMemo } from 'react'
 import { isProduction } from '../constants'
 
-export const logger = (ctx: string) => {
+export const logger = (ctx: string, enabled = true) => {
+  let _enabled = enabled
+
   return {
     log: (...args: Parameters<typeof console.log>) => {
-      if (isProduction) return
+      if (isProduction || !_enabled) return
       console.log(`[${ctx}]`, ...args)
     },
     warn: (...args: Parameters<typeof console.warn>) => {
-      if (isProduction) return
+      if (isProduction || !_enabled) return
       console.warn(`[${ctx}]`, ...args)
     },
     error: (...args: Parameters<typeof console.error>) => {
-      if (isProduction) return
+      if (isProduction || !_enabled) return
       console.error(`[${ctx}]`, ...args)
     },
+    enable: () => {
+      _enabled = true
+    },
+    disable: () => {
+      _enabled = false
+    },
   }
+}
+
+export const useLogger = (ctx: string, enabled = true) => {
+  const _logger = useMemo(() => logger(ctx), [ctx])
+
+  useEffect(() => {
+    if (enabled) {
+      _logger.enable()
+    } else {
+      _logger.disable()
+    }
+  }, [_logger, enabled])
+
+  return _logger
 }
